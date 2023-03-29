c8573357b5722b12eb92ddabbd7348cdc974baa8
NaderZadron <84757131+NaderZadron@users.noreply.github.com>
2023-03-29 17:23:49 -0600

    added successRedirect to auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -65,6 +65,7 @@ router.get("/login", function (req, res) {
 router.post(
   "/login",
   passport.authenticate("local", {
+    successRedirect: "/",
     failureRedirect: "/login",
     failureMessage: true,
   }),
