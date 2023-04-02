906acbfe66c5d80ad6491e2a6ee15526dcb33272
NaderZadron <84757131+NaderZadron@users.noreply.github.com>
2023-04-02 01:18:46 -0600

    fixed login route

--- routes/auth.js
+++ routes/auth.js
@@ -62,16 +62,24 @@ router.get("/login", function (req, res) {
   });
 });
 
-router.post(
-  "/login",
-  passport.authenticate("local", {
-    successRedirect: "/",
-    failureRedirect: "/login",
-  }),
-  (req, res) => {
-    res.status(200).json("Login Success");
-  }
-);
+router.post("/login", (req, res, next) => {
+  passport.authenticate("local", (err, user, info) => {
+    if (err) {
+      return next(err);
+    }
+    if (!user) {
+      return res
+        .status(401)
+        .json({ message: "Username or password incorrect" });
+    }
+    req.logIn(user, (err) => {
+      if (err) {
+        return next(err);
+      }
+      return res.status(200).json({ message: "Login success" });
+    });
+  })(req, res, next);
+});
 
 router.get("/logout", function (req, res) {
   req.logout(function (err) {
