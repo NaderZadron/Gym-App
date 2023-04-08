77d8adda384534868a1b78b7521f09f9835b6f80
Samuel Hilfer <63205868+GrowingPaigns@users.noreply.github.com>
2023-04-08 13:03:14 -0600

    Split up error checking
    Split error checks into two pieces and added some lines that further explain what error occurred

--- middleware/isAdmin.js
+++ middleware/isAdmin.js
@@ -1,12 +1,21 @@
 const User = require("../models/user");
 module.exports.isAdmin = () => {
   return async function (req, res, next) {
+
     const user = await User.findById(req.session.passport.user.id);
-    if (!user || (user.position !== "admin" && user.position !== "coach")) {
-      return res
-        .status(403)
-        .json({ error: { status: 403, message: "Access denied." } });
+
+    if (!user) {
+      console.log("[ERROR - Unable to find User]");
+      return res.status(403).json({ error: { status: 403, message: "Access denied." } });
     }
-    next();
+    else if (user.position !== "admin" && user.position !== "coach") {
+      console.log("[ERROR - User does not have admin or coach position]");
+      return res.status(403).json({ error: { status: 403, message: "Access denied." } });
+    }
+    else {
+      console.log("[isAdmin successfully verified]");
+      next();
+    }
+
   };
 };
